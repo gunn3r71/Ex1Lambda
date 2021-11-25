64c0c4cdf0d52023a7d98d1e3ec611bd6552ba7a
Lucas <lucasgunner3@gmail.com>
2021-11-25 20:57:13 -0300

    Adicionar arquivos de projeto.

---
+++ Ex1Lambda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex1Lambda
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<double> elements = new()
+            {
+                3,7,2,4,6
+            };
+
+            elements = elements.ConvertAll(x => x / 2);
+
+            elements.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
